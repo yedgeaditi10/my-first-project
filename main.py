b4f02533585a9cd77d8d6c8476364a0f9b29b3af
Aditi <aditiyedge10@gmail.com>
2026-03-20 10:37:48 +0530

    new change

--- main.py
+++ main.py
@@ -1,7 +1,17 @@
-def fib(n):
-    a, b = 0, 1
-    while a < n:
-        print(a, end=' ')
-        a, b = b, a+b
+# Iterative approach to generate Fibonacci series
+def fibonacci_series(n):
+    series = [0, 1]
+    if n <= 0:
+        return []
+    elif n == 1:
+        return [0]
+    
+    # Generate subsequent numbers
+    for i in range(2, n):
+        next_num = series[i-1] + series[i-2]
+        series.append(next_num)
+    return series
 
-fib(1000) # Outputs Fibonacci series up to 1000
+# Example: Print the first 10 terms
+print(fibonacci_series(10)) 
+# Output: [0, 1, 1, 2, 3, 5, 8, 13, 21, 34]
